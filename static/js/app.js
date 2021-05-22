b23c12fcea836b7355c9fe9736d226f5a2e1e7fc
Tina Zhou <haiyun.zhou2010@gmail.com>
2021-05-21 17:41:24 -0700

    bubble problem

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,6 @@
 // Use D3 fetch to read the JSON file
 // The data from the JSON file is arbitrarily named importedData as the argument
+function buildPlot(){
 d3.json("samples.json").then((importedData) => {
     console.log(importedData)
     var names = importedData.names;
@@ -24,16 +25,16 @@ d3.json("samples.json").then((importedData) => {
         dataset.sort(function (a, b) {
             return parseFloat(sample_values[b]) - parseFloat(sample_values[a]);
         });
-        console.log(dataset[0].sample_values)
+        // console.log(dataset[0].sample_values)
         // Slice the first 10 objects for plotting
         // Reverse the array due to Plotly's defaults
         var topTenValues = dataset[0].sample_values.slice(0, 10).reverse();
-        console.log("value:" + topTenValues)
+        // console.log("value:" + topTenValues)
         var topTenOtuIds = dataset[0].otu_ids.slice(0, 10).reverse()
         topTenOtuIds = topTenOtuIds.map(topTenOtuId => 'OTU ' + topTenOtuId)
-        console.log("id: " + topTenOtuIds)
+        // console.log("id: " + topTenOtuIds)
         var topTenOtuLables = dataset[0].otu_labels.slice(0, 10).reverse()
-        console.log("lables: " + topTenOtuLables)
+        // console.log("lables: " + topTenOtuLables)
 
         // Trace1 for the sample Data
         var trace1 = {
@@ -68,19 +69,19 @@ d3.json("samples.json").then((importedData) => {
         // Trace2 for the sample Data
         var trace2 = {
             x: dataset[0].otu_ids,
-            y: parseFloat(dataset[0].sample_values),
+            y: dataset[0].sample_values,
             text: dataset[0].otu_labels,
             mode: "markers",
             marker: {
-                // size:Math.sqrt(dataset[0].sample_values),
-                // size:2* math.max(dataset[0].sample_values)/(100**2),
                 size: dataset[0].sample_values,
                 color: dataset[0].otu_ids,
-                // showScale:True
             }
 
         };
-
+        console.log(dataset[0].sample_values)
+        console.log(dataset[0].otu_ids)
+        console.log(dataset[0].otu_labels)
+        
         // data
         var chartData2 = [trace2];
 
@@ -98,6 +99,7 @@ d3.json("samples.json").then((importedData) => {
         // Render the plot to the div tag with id "plot"
         Plotly.newPlot("bubble", chartData2, layout)
         
+        // get demographic info for the chosen ID
         var demoInfo = metadata.filter(metadata => metadata.id == chosenId);
         var meta = d3.select("#sample-metadata").html("").append("ul");
         Object.entries(demoInfo[0]).forEach(([key, value]) => {
@@ -105,3 +107,5 @@ d3.json("samples.json").then((importedData) => {
         })
     }
 })
+}
+buildPlot()
